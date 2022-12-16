e3900cc0489efeeb9f3d512bf239f3c8b9c5948d
MeTalent <me@MeTalents-MBP.fritz.box>
2022-12-16 17:14:09 +0100

    --no-sandbox

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ export async function nodeHtmlToImage(options: Options) {
     selector,
     type,
     quality,
-    puppeteerArgs = {},
+    puppeteerArgs = { args: ["--no-sandbox"] },
     puppeteer = undefined,
   } = options;
 
